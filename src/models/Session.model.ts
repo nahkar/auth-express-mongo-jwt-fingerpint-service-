3d6b5a74f3fae75357f2d450089eec7ed564e6c9
Belenov Dmitriy <dimabelenov@gmail.com>
2023-02-02 12:22:33 +0200

    add session schema

---
+++ src/models/Session.model.ts
@@ -0,0 +1,36 @@
+import { Schema, Types, model } from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const SessionSchema = new Schema({
+	userId: {
+		type: Types.ObjectId,
+		ref: 'User',
+		required: true,
+	},
+	refreshToken: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	ip: {
+		type: String,
+		required: true,
+	},
+	expiresAt: {
+		type: Date,
+		required: true,
+		default: new Date(
+			new Date().setDate(
+				new Date().getDate() + Number(process.env.SESSION_EXPIRES_DAYS)
+			)
+		),
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+});
+
+export const User = model('Session', SessionSchema);
