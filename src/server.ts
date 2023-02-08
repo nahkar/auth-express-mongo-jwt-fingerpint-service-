367f6c9d60d4b8f663ce87d2cd909a561f9b1b50
Belenov Dmitriy <dimabelenov@gmail.com>
2023-02-08 12:22:33 +0200

    change pathes in ts-config and move routes in v1 folder

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,19 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 
-import { connectToDatabase } from 'services/db';
-import { router } from './routes';
 import { apiErrorMiddleware } from '@middlewares/error.middleware';
+import { connectToDatabase } from '@services/db';
+import { router } from '@routes/v1';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3060;
 
 const app = express();
+
 app.use(express.json());
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
 	res.json({ message: 'Auth API, please go to /api/v1/auth' });
