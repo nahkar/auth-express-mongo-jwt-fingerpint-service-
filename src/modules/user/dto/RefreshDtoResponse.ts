886c0e3d79926e68bae0339bcf228fb00c2a5d8e
Belenov Dmitriy <dimabelenov@gmail.com>
2023-02-27 21:36:38 +0200

    change userDtoResponse for getting isActivated field

--- src/modules/user/dto/RefreshDtoResponse.ts
+++ src/modules/user/dto/RefreshDtoResponse.ts
@@ -1,3 +1,4 @@
+import type { IActivate } from '@interfaces/acivate.interface';
 import type { IUser } from '@interfaces/user.interface';
 
 export class RefreshDtoResponse {
@@ -5,10 +6,12 @@ export class RefreshDtoResponse {
 	email: string;
 	id: string;
 	phone?: string;
-	constructor(model: IUser & { accessToken: string }) {
+	isActivated: boolean;
+	constructor(model: IUser & { accessToken: string }&  Pick<IActivate, 'isActivated'>) {
 		this.accessToken = model.accessToken;
 		this.id = model._id.toString();
 		this.email = model.email;
 		this.phone = model.phone;
+		this.isActivated = model.isActivated;
 	}
 }
