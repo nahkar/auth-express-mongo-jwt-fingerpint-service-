c6e766755abff8a7ca0e28e507e9ef271f6ce605
Belenov Dmitriy <dimabelenov@gmail.com>
2023-02-28 21:51:39 +0200

    add redirect for the activation route

--- src/modules/activate/activate.controller.ts
+++ src/modules/activate/activate.controller.ts
@@ -1,16 +1,17 @@
-import httpStatus from 'http-status';
+import dotenv from 'dotenv';
 
 import { activateService } from './activate.service';
 
 import type { NextFunction, Request, Response } from 'express';
 
+dotenv.config();
+
 class ActivateController {
 	async activate(req: Request, res: Response, next: NextFunction) {
 		const { code } = req.params;
-
 		try {
 			await activateService.activate(code);
-			res.status(httpStatus.OK).json({ msg: 'You have been activated successfully.' });
+			res.redirect(`${process.env.CLIENT_URL ?? ''}/activate-success`);
 		} catch (error) {
 			next(error);
 		}
